a9ba8d45b2fe4ecddd83a68e9e72485a321a71d9
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-06-09 14:07:30 +0300

    ref: use memo for intervals

--- components/timeline/Timeline.tsx
+++ components/timeline/Timeline.tsx
@@ -2,7 +2,7 @@ import { createIntervals } from '@/helpers/createRenderMap';
 import { getColor } from '@/helpers/getColor';
 import { getTotalTime } from '@/helpers/getTotalTime';
 import { ITimeValues } from '@/shared/types';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import styles from './styles.module.scss';
 
 type Timeline = {
@@ -10,8 +10,6 @@ type Timeline = {
 };
 
 export function Timeline({ values }: Timeline) {
-  const totalTime = getTotalTime(values);
-
   const timelineRef = useRef<HTMLDivElement>(null);
   const [timelineWidth, setTimelineWidth] = useState(0);
 
@@ -20,13 +18,11 @@ export function Timeline({ values }: Timeline) {
     setTimelineWidth(timelineRef.current.offsetWidth);
   }, []);
 
-  const singleSecondWidth = timelineWidth / totalTime;
-
-  console.log('timelineWidth :>> ', timelineWidth);
-  console.log('totalTime', totalTime);
-  console.log('singleSecondWidth', singleSecondWidth);
-
-  const intervals = createIntervals(values, singleSecondWidth);
+  const intervals = useMemo(() => {
+    const totalTime = getTotalTime(values);
+    const singleSecondWidth = timelineWidth / totalTime;
+    return createIntervals(values, singleSecondWidth);
+  }, [values, timelineWidth]);
 
   return (
     <div ref={timelineRef} className={styles.timeline_container}>
